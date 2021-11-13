eb4def3a637312777bf419901f5995983679210a
muradabdi <me@muradabdi.com>
2021-11-13 14:08:13 -0500

    Syscall to get text from input

--- project2.s
+++ project2.s
@@ -6,3 +6,7 @@
 
 .text
 	main:											# Start of code section
+		li $v0, 8									# 8 = code to read string
+		la $a0, userInput							# Load userInput address into $a0
+		li $a1, 1000								# Load 1000 into $a1
+		syscall
