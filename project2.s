0add4f87c2099d13860facb9f62cafec1b858dc0
muradabdi <me@muradabdi.com>
2021-11-13 14:06:02 -0500

    Set length of userInput, error message to print

--- project2.s
+++ project2.s
@@ -1,6 +1,8 @@
 # PROGRAM: CSCI 201 MIPS Programming Project 2
 
 .data
+	error: .asciiz "Not recognized"					# Error message 
+	userInput: .space 1000							# Set input character limit
 
 .text
-	main:					# Start of code section
+	main:											# Start of code section
